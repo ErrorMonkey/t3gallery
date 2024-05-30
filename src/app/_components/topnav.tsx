dc2656e47ab4885cbcdc5d5f6b70cb113aeabd66
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-05-31 02:24:54 +0900

    0530: uploadthing

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
+  const router = useRouter();
+
   return (
     <nav
       className="flex w-full items-center justify-between border-b p-4 text-xl
@@ -8,11 +14,17 @@ export default function TopNav() {
     >
       <div>Gallery</div>
 
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,21 @@ async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+
   return (
     <div className="flex flex-wrap gap-4">
-      {images.map((post) => (
-        <div key={post.id}></div>
+      {images.map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <img src={image.url} alt="" />
+          <div>{image.name}</div>
+        </div>
       ))}
-      {[...images, ...images, ...images].map((image, index) => (
+      {/* {[...images, ...images, ...images].map((image, index) => (
         <div key={image.id + "-" + index} className="flex w-48 flex-col">
           <img alt="" src={image.url} />
           <div>{image.name}</div>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
@@ -29,7 +33,9 @@ export default async function HomePage() {
     <>
       <main className="">
         {/* <TestPage /> */}
-        <SignedOut>Pls Sign in</SignedOut>
+        <SignedOut>
+          <div className="h-full w-full text-center text-2xl">Pls Sign in</div>
+        </SignedOut>
         <SignedIn>
           <Images />
         </SignedIn>
